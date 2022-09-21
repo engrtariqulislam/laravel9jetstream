b38cef835e501833bff2a785c8ec9ead92be1fcd
Tariqul islam <tnawayn83@yahoo.com>
2022-09-22 02:57:51 +0300

    Laravel 9 Multi Auth Part 1
    Laravel 9 Multi Auth Part 1

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use App\Actions\Fortify\AttemptToAuthenticate;
+use App\Actions\Fortify\RedirectIfTwoFactorAuthenticatable;
+use App\Http\Controllers\AdminController;
+use Auth;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -21,7 +26,9 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->when([AdminController::class,AttemptToAuthenticate::class,RedirectIfTwoFactorAuthenticatable::class])->needs(StatefulGuard::class)->give(function (){
+            return Auth::gurad('admin');
+    });
     }
 
     /**
